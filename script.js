761afd39f8fef0c595efca7647e87c9e8a290c8e
archiepv <archiepv@uw.edu>
2018-07-30 19:15:02 -0700

    update exercise

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("let's build some charts!")
+      // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawAllocationsChart() {
+
+        // Create the data table.
+				// Two times 0-100, five times 100-200, three times 200-300
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Score Range');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['0-100', 2],
+          ['100-200', 5],
+          ['200-300', 3]
+          
+        ]);
+
+        // Set chart options
+        var options = {'title':'Bowling Score',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('allocations'));
+        chart.draw(data, options);
+      }
