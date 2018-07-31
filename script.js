aa1e25825fe208f8ab32d5b8425aac83f7f86ab6
archiepv <archiepv@uw.edu>
2018-07-30 19:48:50 -0700

    Added new div and function for a bar chart

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
       // Set a callback to run when the Google Visualization API is loaded.
       google.charts.setOnLoadCallback(drawChart);
 			google.charts.setOnLoadCallback(drawAllocationsChart);
+			google.charts.setOnLoadCallback(drawSemesterChart);
 
       // Callback that creates and populates a data table,
       // instantiates the pie chart, passes in the data and
@@ -23,7 +24,7 @@
         ]);
 
         // Set chart options
-        var options = {'title':'Bowling Score',
+        var options = {'title':'Pizza Toppings',
                        'width':400,
                        'height':300};
 
@@ -31,26 +32,57 @@
         var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
         chart.draw(data, options);
       }
-function drawAllocationsChart() {
+
+			function drawAllocationsChart() {
 
         // Create the data table.
 				// Two times 0-100, five times 100-200, three times 200-300
         var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Score Range');
-        data.addColumn('number', 'Slices');
+        data.addColumn('string', 'Expense');
+        data.addColumn('number', 'Cents');
         data.addRows([
-          ['0-100', 2],
-          ['100-200', 5],
-          ['200-300', 3]
+          ['Administrative Costs', 4],
+          ['Fundraising', 16],
+					['Youth Programs', 36],
+          ['Adult Programs', 44]
           
         ]);
 
         // Set chart options
-        var options = {'title':'Bowling Score',
+        var options = {'title':'Each Dollar Donated Goes To',
                        'width':400,
-                       'height':300};
+                       'height':300,
+											 'colors': ['#8AD1C2', '#9F8AD1', '#D18A99', '#BCD18A'],
+											 'pieHole': 0.5};
 
         // Instantiate and draw our chart, passing in some options.
         var chart = new google.visualization.PieChart(document.getElementById('allocations'));
+        chart.draw(data, options);
+      }
+
+			function drawSemesterChart() {
+
+        // Create the data table.
+				// Two times 0-100, five times 100-200, three times 200-300
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Expense');
+        data.addColumn('number', 'Cents');
+        data.addRows([
+          ['Administrative Costs', 4],
+          ['Fundraising', 16],
+					['Youth Programs', 36],
+          ['Adult Programs', 44]
+          
+        ]);
+
+        // Set chart options
+        var options = {'title':'Each Dollar Donated Goes To',
+                       'width':400,
+                       'height':300,
+											 'colors': ['#8AD1C2', '#9F8AD1', '#D18A99', '#BCD18A'],
+											 'pieHole': 0.5};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('semester'));
         chart.draw(data, options);
       }
